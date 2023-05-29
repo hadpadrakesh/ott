10e0b5290ee4b655db1788d8dba62854a385d927
hadpadrakesh <134768183+hadpadrakesh@users.noreply.github.com>
2023-05-29 20:04:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 
-// import Addmovie from "./component/Addmovie";
-// import Home from "./component/Home";
-// import Moviedetails from "./component/Moviedetails";
-// import Navbar from "./component/Navbar";
-// import {BrowserRouter, Routes, Route} from 'react-router-dom'
-// import Favorites from "./component/Favorites";
-// import Search from "./component/Search";
-// import Updatemovie from "./component/Updatemovie";
+import Addmovie from "./component/Addmovie";
+import Home from "./component/Home";
+import Moviedetails from "./component/Moviedetails";
+import Navbar from "./component/Navbar";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Favorites from "./component/Favorites";
+import Search from "./component/Search";
+import Updatemovie from "./component/Updatemovie";
 
-import Parent from "./Spotify/Parent";
 
-// import Navbar1 from "./Spotify/Navbar1";
 
 // import Addproducts from "./Assignment/Addproducts";
 // import Navpage from "./Assignment/Navpage";
@@ -22,27 +20,27 @@ import Parent from "./Spotify/Parent";
 
 // !Movie
 
-// function app()
-// {
-//     return(
-//       <BrowserRouter>
-//         <div className="App">
-//           <Navbar/>
-//           <Routes>
-//             <Route path="/" element={<Home/>}/>      
-//             <Route path="/add" element={<Addmovie/>}/>
-//             <Route path="/moviedetails/:id" element={<Moviedetails/>}/>
-//             <Route path="/fav" element={<Favorites/>}/>
-//             <Route path="/search/:searchword" element={<Search/>}/> 
-//             <Route path="/update/:id" element={<Updatemovie/>}/>      
-//           </Routes>
+function app()
+{
+    return(
+      <BrowserRouter>
+        <div className="App">
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>      
+            <Route path="/add" element={<Addmovie/>}/>
+            <Route path="/moviedetails/:id" element={<Moviedetails/>}/>
+            <Route path="/fav" element={<Favorites/>}/>
+            <Route path="/search/:searchword" element={<Search/>}/> 
+            <Route path="/update/:id" element={<Updatemovie/>}/>      
+          </Routes>
           
-//         </div>
-//         </BrowserRouter>
+        </div>
+        </BrowserRouter>
        
-//     )
-// }
-// export default app;
+    )
+}
+export default app;
 
 
 // //!product
@@ -80,15 +78,3 @@ import Parent from "./Spotify/Parent";
 // }
 // export default app;
 
-function app ()
-{
-    return (
-
-      <div className="app">
-
-      <Parent/>
-
-      </div>
-    )
-}
-export default app;
